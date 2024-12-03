a836041ababf7a6d0b92d0a71e89a7db341e0d47
Aryan Rajesh <aryanrajesh6702@gmail.com>
2024-12-03 11:23:22 +0530

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,27 +1,49 @@
-import random
+import secrets
+import string
 import pyfiglet
-app=pyfiglet.figlet_format("Password Generator")
+
 def password_generator():
+    app = pyfiglet.figlet_format("Password Generator")
     print(app)
-    lst1=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y", "z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    lst2=[0,1,2,3,4,5,6,7,8,9]
-    lst3=["`","!","@","#","$","%","^","&","*","(",")","_","-","+","=",",","<",".",">","?","/","'",":",";","{","[","}","]"]
-    n=int(input("Enter the number of letters: "))
-    a=int(input("Enter the number of digits: "))
-    b=int(input("Enter the number of special characters: "))
-    password=""
-    for i in range(n):
-        password+=str(random.choice(lst1))
-    for i in range(a):
-        password+=str(random.choice(lst2))
-    for i in range(b):
-        password+=str(random.choice(lst3))
-    print(f"Generated Password is: {password}")
-while(True):
-    choice=input("Do you want to continue generating the passwords: ")
-    if(choice=="yes"):
+
+    # Define the character sets for password generation
+    lst1 = string.ascii_letters  # a-zA-Z
+    lst2 = string.digits          # 0-9
+    lst3 = string.punctuation     # Special characters
+    
+    # Take user input for the total length and other password parameters
+    n = int(input("Enter the number of letters: "))
+    a = int(input("Enter the number of digits: "))
+    b = int(input("Enter the number of special characters: "))
+    
+    total_length = n + a + b
+    if total_length < 12:  # Password length check
+        print("For better security, the password should be at least 12 characters long.")
+        return
+
+    # Generate the password parts
+    password = [
+        secrets.choice(lst1) for _ in range(n)
+    ] + [
+        secrets.choice(lst2) for _ in range(a)
+    ] + [
+        secrets.choice(lst3) for _ in range(b)
+    ]
+    
+    # Shuffle the password to make it more secure
+    secrets.SystemRandom().shuffle(password)
+
+    # Join the list into a string
+    final_password = ''.join(password)
+
+    print(f"Generated Password is: {final_password}")
+
+while True:
+    choice = input("Do you want to continue generating passwords (yes/no)? ").strip().lower()
+    if choice == "yes":
         password_generator()
-    else:
+    elif choice == "no":
         print("Thank you for using our application!")
         break
-
+    else:
+        print("Please enter 'yes' or 'no'.")
